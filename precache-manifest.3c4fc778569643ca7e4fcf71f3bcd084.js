3d11612cf09dc4d068b6fc583b6679753df0ec36
YASH-CODE1 <yashdivine21@gmail.com>
2022-08-13 16:43:14 +0530

    Updates

---
+++ precache-manifest.3c4fc778569643ca7e4fcf71f3bcd084.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "762d19e5782b3cca1104",
+    "url": "/cineflix_by_yash/static/css/main.fc3c46f3.chunk.css"
+  },
+  {
+    "revision": "762d19e5782b3cca1104",
+    "url": "/cineflix_by_yash/static/js/main.762d19e5.chunk.js"
+  },
+  {
+    "revision": "cb254b9f162f175d5000",
+    "url": "/cineflix_by_yash/static/js/1.cb254b9f.chunk.js"
+  },
+  {
+    "revision": "5c262c25c34567e439af",
+    "url": "/cineflix_by_yash/static/js/runtime~main.5c262c25.js"
+  },
+  {
+    "revision": "b98b9440cdb944acae3b80ee58cdbfe7",
+    "url": "/cineflix_by_yash/static/media/search.b98b9440.svg"
+  },
+  {
+    "revision": "2911208380d8b57e80102d5fda295777",
+    "url": "/cineflix_by_yash/static/media/4.29112083.jpg"
+  },
+  {
+    "revision": "9e126a75037383f89abdff74334a2fef",
+    "url": "/cineflix_by_yash/index.html"
+  }
+];
